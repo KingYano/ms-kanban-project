b3ddf350b6fac68fc789c6926c62128e0588fda9
KingYano <belkadi.yanis.l@gmail.com>
2024-09-28 00:11:53 +0200

    installation sass

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,10 +1,12 @@
 <template>
-  <div>
-    <KanbanBoard></KanbanBoard>
-  </div>
+  <KanbanBoard></KanbanBoard>
 </template>
 
 
 <script setup lang="ts">
-import KanbanBoard from "~/components/KanbanBoard/KanbanBoard.vue";
+  import KanbanBoard from "~/components/KanbanBoard/KanbanBoard.vue";
 </script>
+
+<style lang="scss">
+  @use '@/assets/styles/main.scss' as *;
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
   srcDir: 'frontend/',
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/main.scss" as *;'
+        }
+      }
+    }
+  }
 })
